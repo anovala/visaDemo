e9ecf8f8f8b41e053cf3a36f9eb3e8a522fe0912
anovala <386147706@qq.com>
2024-07-29 10:59:46 +0800

    update

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 #include "src/base.h"
 #include <string>
 #include <visa.h>
+#include <iostream>
 
 int main(void)
 {
-    std::string resource("TCPIP::192.168.1.42::INSTR"); 
+    std::cout << "IP:"; 
+    std::string IP;
+    std::cin >> IP;
+
+    std::string resource;//"TCPIP::192.168.1.42::INSTR
+    resource = "TCPIP::"+IP+"::INSTR";
     std::string png;
     std::string path;
     std::string absPng;
